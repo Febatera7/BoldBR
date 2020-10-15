bc7e0cd5263a9c351d6ca5e556664025ed1ce279
Felipe Nunes <febatera.soares@gmail.com>
2020-10-15 20:24:18 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -7,7 +7,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://192.168.99.100:27017/bolddatabase',
+      'mongodb+srv://boldbr:<password>@boldbr.ucvp3.mongodb.net/<dbname>?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useFindAndModify: true,
